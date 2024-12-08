9c7a9ca1f96402d572b9ddef7fb5f74503c83507
leo silva <leo@pop-os.localdomain>
2024-12-07 20:30:06 -0400

    tentativa de reversao

--- index.js
+++ index.js
@@ -28,92 +28,44 @@ if ((process.env.NODE_ENV = "development")) {
 app.get("/api/stream/:cam", (req, res) => {
   const { cam } = req.params;
   console.log("cam :>> ", cam);
+  // res.setHeader("content-type", "video/x-flv");
+  // const stat = fs.statSync(filePath);
+  // res.setHeader("content-length", stat.size);
+  // const fileStream = fs.createReadStream(filePath);
+  // fileStream.on("error", error => handleError(error, res));
+  // fileStream.pipe(res)
 
+  // res.setHeader("content-type", "video/x-flv");
   const url = cams[cam]?.url;
   if (!url) return res.end();
 
-  const options = {};
+  res.contentType("video/mp4");
+  const cmd = url.startsWith("rtsp")
+    ? ffmpeg(url).inputOptions("-rtsp_transport udp")
+    : ffmpeg(url);
+  cmd
+    .format("matroska")
+    .on("start", (commandLine) => {
+      console.log("Spawned Ffmpeg with command: " + commandLine);
+    })
+    .on("error", (err, stdout, stderr) => {
+      // console.log("error on cam", cam);
+      console.error("error: " + err.message + "on cam " + cam);
+      cmd.kill();
+      return res.end();
+    })
+    .on("codecData", ({ format, video, video_details }) => {
+      console.log("format :>> ", format);
+      console.log("video :>> ", video);
+      console.log("res :>> ", video_details.at(-5));
+      console.log("fps :>> ", video_details.at(-4));
+      console.log("");
+    })
+    .on("end", () => {
+      console.log("end");
+    });
 
-  let start;
-  let end;
-
-  const range = req.headers.range;
-  if (range) {
-    const bytesPrefix = "bytes=";
-    if (range.startsWith(bytesPrefix)) {
-      const bytesRange = range.substring(bytesPrefix.length);
-      const parts = bytesRange.split("-");
-      if (parts.length === 2) {
-        const rangeStart = parts[0] && parts[0].trim();
-        if (rangeStart && rangeStart.length > 0) {
-          options.start = start = parseInt(rangeStart);
-        }
-        const rangeEnd = parts[1] && parts[1].trim();
-        if (rangeEnd && rangeEnd.length > 0) {
-          options.end = end = parseInt(rangeEnd);
-        }
-      }
-    }
-  }
-
-  res.contentType("video/x-matroska");
-
-  if (req.method === "HEAD") {
-    res.statusCode = 200;
-    //  res.setHeader("accept-ranges", "bytes");
-    // res.setHeader("content-length", contentLength);
-    res.end();
-  } else {
-    // let retrievedLength;
-    // if (start !== undefined && end !== undefined) {
-    //   retrievedLength = end + 1 - start;
-    // } else if (start !== undefined) {
-    //   retrievedLength = contentLength - start;
-    // } else if (end !== undefined) {
-    //   retrievedLength = end + 1;
-    // } else {
-    //   retrievedLength = contentLength;
-    // }
-
-    //  res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
-
-    // res.setHeader("content-length", retrievedLength);
-
-    //  if (range !== undefined) {
-    //    res.setHeader(
-    //      "content-range",
-    //      `bytes ${start || 0}-${end || contentLength - 1}/${contentLength}`
-    //    );
-    //    res.setHeader("accept-ranges", "bytes");
-    //  }
-
-    //   res.contentType("video/mp4");
-    const cmd = url.startsWith("rtsp")
-      ? ffmpeg(url).inputOptions("-rtsp_transport udp")
-      : ffmpeg(url);
-    cmd
-      .format("matroska")
-      .on("start", (commandLine) => {
-        console.log("Spawned Ffmpeg with command: " + commandLine);
-      })
-      .on("error", (err, stdout, stderr) => {
-        console.error("error: " + err.message + "on cam " + cam);
-        cmd.kill();
-        return res.end();
-      })
-      .on("codecData", ({ format, video, video_details }) => {
-        console.log("format :>> ", format);
-        console.log("video :>> ", video);
-        console.log("res :>> ", video_details.at(-5));
-        console.log("fps :>> ", video_details.at(-4));
-        console.log("");
-      })
-      .on("end", () => {
-        console.log("end");
-      });
-
-    return cmd.pipe(res, { end: true });
-  }
+  return cmd.pipe(res, { end: true });
 });
 
 app.get("/api/cams", (_, res) => {
